63d7ba010d66ab43c2509f4d7b89e30aee9ecf00
Dipendra Kumar Shah <dipendrashah0789878@gmail.com>
2023-01-19 22:29:08 +0530

    first commit

---
+++ src/app/inbox/inbox.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inbox',
+  templateUrl: './inbox.component.html',
+  styleUrls: ['./inbox.component.css']
+})
+export class InboxComponent {
+
+}
